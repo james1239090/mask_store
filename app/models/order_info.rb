a2e010e400d82705930ec16f04ee3f7d9a833e01
James <james1239090@gmail.com>
2017-04-10 17:33:08 +0800

    billing and shipping name and address sholud not be blank

--- app/models/order_info.rb
+++ app/models/order_info.rb
@@ -1,3 +1,8 @@
 class OrderInfo < ApplicationRecord
   belongs_to :order, optional: true
+
+  validates :billing_name, presence: true
+  validates :billing_address, presence: true
+  validates :shipping_name, presence: true
+  validates :shipping_address, presence: true
 end
