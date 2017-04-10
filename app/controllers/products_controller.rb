3d87f66fd985cd3274b71b4f4cef8548b54b5546
James <james1239090@gmail.com>
2017-04-10 16:04:54 +0800

    add flash notice to show item added

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,7 +10,7 @@ class ProductsController < ApplicationController
   def add_to_cart
     @product = Product.find(params[:id])
     current_cart.add_product_to_cart(@product)
-
+    flash[:notice] = "你已成功將#{@product.title} 加入購物車"
     redirect_to :back
   end
 end
