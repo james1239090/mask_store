fd7f30c66a95c392804bd3b6dc108be454cf9a8b
James <james1239090@gmail.com>
2017-04-11 14:11:45 +0800

    order can change state by button

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -3,6 +3,7 @@ class Admin::OrdersController < ApplicationController
 
   before_action :authenticate_user!
   before_action :admin_required
+  before_action :find_order, on: [:ship,:shipped,:cancel,:return]
 
   def index
     @orders = Order.order("id DESC")
@@ -12,6 +13,31 @@ class Admin::OrdersController < ApplicationController
     @order = Order.find(params[:id])
     @order_info = @order.info
     @order_items = @order.items
+  end
+
+  def ship
+    @order.ship!
+    redirect_to :back
+  end
+
+  def shipped
+    @order.deliver!
+    redirect_to :back
+  end
+
+  def cancel
+    @order.cancell_order!
+    redirect_to :back
+  end
+
+  def return
+    @order.return_good!
+    redirect_to :back
+  end
+
+  private
+  def find_order
+    @order = Order.find(params[:id])
   end
 
 end
