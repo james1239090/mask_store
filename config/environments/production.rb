7fecb19fdbe9ada47909dfe892029cd17267a23f
James <james1239090@gmail.com>
2017-04-14 13:52:45 +0800

    change the billing mode from production to development on production.rb for testing payment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,7 +85,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.after_initialize do
-    ActiveMerchant::Billing::Base.mode = :production
+    ActiveMerchant::Billing::Base.mode = :development
   end
 
   config.action_mailer.delivery_method = :smtp
