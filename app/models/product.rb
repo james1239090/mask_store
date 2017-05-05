2ae8e452491d47fc4feaff44fa0ec90e37131bf7
James <james1239090@gmail.com>
2017-05-05 10:26:14 +0800

    fix drop zone with dynamic option could create and update

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,7 +5,7 @@ class Product < ApplicationRecord
   has_many :colors, :through => :product_options, source: :color
 
   accepts_nested_attributes_for :photo, :allow_destroy => true
-  accepts_nested_attributes_for :product_options, :allow_destroy => true
+  accepts_nested_attributes_for :product_options, :allow_destroy => true,:reject_if => :all_blank
   accepts_nested_attributes_for :dimensions, :allow_destroy => true
   accepts_nested_attributes_for :colors, :allow_destroy => true
 end
