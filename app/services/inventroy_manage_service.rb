fd5767e03b1855cc884b32e5a7f445302a79edde
James <james1239090@gmail.com>
2017-05-31 21:19:57 +0800

    fix the wrong sequence of variable of getInventory

--- app/services/inventroy_manage_service.rb
+++ app/services/inventroy_manage_service.rb
@@ -5,8 +5,10 @@ class InventroyManageService
 
   def new_purchase!
     @source.purchase_items.each do |purchase_item|
-      @inventory = Inventory.getInventory(purchase_item.product_id, purchase_item.dimension_id, purchase_item.color_id)
-      if @inventory.blank?
+      @inventory = Inventory.getInventory(purchase_item.product_id,purchase_item.color_id , purchase_item.dimension_id)
+      puts Inventory.getInventory(purchase_item.product_id,purchase_item.color_id ,  purchase_item.dimension_id)
+
+      if @inventory.count == 0
         Inventory.add_inventory_from_purchase_item(purchase_item)
       else
         @inventory.first.new_calculate_average_price(purchase_item)
@@ -25,14 +27,14 @@ class InventroyManageService
 
   def new_sale!
     @source.sale_items.each do |sale_item|
-      @inventory = Inventory.getInventory(sale_item.product_id, sale_item.dimension_id, sale_item.color_id).first
+      @inventory = Inventory.getInventory(sale_item.product_id, sale_item.color_id, sale_item.dimension_id).first
       @inventory.sale_inventory_from_sale_item(sale_item)
     end
   end
 
   def update_sale!
     @source.sale_items.each do |sale_item|
-      @inventory = Inventory.getInventory(sale_item.product_id, sale_item.dimension_id, sale_item.color_id).first
+      @inventory = Inventory.getInventory(sale_item.product_id, sale_item.color_id, sale_item.dimension_id).first
       @inventory.update_sale_from_item(sale_item)
     end
   end
