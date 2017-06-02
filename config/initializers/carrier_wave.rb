b11098891f3531719a368b4e1d507ef9e1ecbe4c
James <james1239090@gmail.com>
2017-06-02 16:46:06 +0800

    fix the carrierwave

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,12 +1,12 @@
 CarrierWave.configure do |config|
   if Rails.env.production?
-    config.storage :fog
     config.fog_credentials = {
       provider: 'AWS',
       aws_access_key_id: ENV['aws_access_key_id'],
       aws_secret_access_key: ENV['aws_secret_access_key'],
       region: 'ap-northeast-1'
     }
+    config.storage :fog
     config.fog_directory = 'maskstore'
   else
     config.storage :file
