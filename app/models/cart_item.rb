a516476e844a8a0a842fe85c5a746eda9098140f
James <james1239090@gmail.com>
2017-07-05 15:38:23 +0800

    add the validates for cart_item

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -3,4 +3,15 @@ class CartItem < ApplicationRecord
   belongs_to :product
   belongs_to :color
   belongs_to :dimension
+
+  validates :product,:color,:dimension, :quantity ,presence: true
+  validates :quantity, numericality: {less_than_or_equal_to: :getInventoryQty}, presence: true
+
+
+  private
+  def getInventoryQty
+    inventory = Inventory.getInventory(self.product_id, self.color_id, self.dimension_id).first
+    quantity = inventory.quantity
+    quantity
+  end
 end
