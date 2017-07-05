ff04145163ffc2d64e82b9becaefc6e9268a160d
James <james1239090@gmail.com>
2017-07-05 15:46:31 +0800

    make sure user select the color and dimension and quantity should less_than_or_equal_to inventory

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,10 +9,15 @@ class ProductsController < ApplicationController
 
   def add_to_cart
     @product = Product.find(params[:id])
-    @color = params[:cart][:cart_items][:color_id]
-    @dimension = params[:cart][:cart_items][:dimension_id]
-    current_cart.add_product_to_cart(@product,@color,@dimension)
-    flash[:notice] = "你已成功將#{@product.title} 加入購物車"
+    if params.has_key?(:color_id) && params.has_key?(:dimension_id)
+      @color = params[:cart][:cart_items][:color_id]
+      @dimension = params[:cart][:cart_items][:dimension_id]
+      current_cart.add_product_to_cart(@product,@color,@dimension)
+      flash[:notice] = "你已成功將#{@product.title} 加入購物車"
+    else
+      flash[:warning] = "請選擇顏色或尺吋"
+    end
+
     redirect_to :back
   end
 end
