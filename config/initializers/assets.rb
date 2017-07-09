3dbc3cde87e232856db375f516b7b75517de5f59
James <james1239090@gmail.com>
2017-07-09 14:57:47 +0800

    use precomplier for heroku of proudction

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,4 +8,15 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-# Rails.application.config.assets.precompile += %w( search.js )
+
+
+%w(account/orders admin/inventories admin/orders admin/photos admin/product_oprtions
+   admin/products admin/purchases admin/sale_platforms admin/sales admin/users
+   admin cart_items carts orders products).each do |controller|
+  Rails.application.config.assets.precompile += [
+      "#{controller}.coffee",
+      "#{controller}.js.coffee",
+      "#{controller}.css"
+  ]
+  Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf)\z/
+end
